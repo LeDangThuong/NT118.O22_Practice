892b1e10f592240d4f4c5cd8e2d8189b96ad15e5
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-03-20 09:27:47 +0700

    Add lab_01

---
+++ Lab_1/NT118.O21.1/app/src/main/java/com/example/nt118o211/FrameLayout.java
@@ -0,0 +1,13 @@
+package com.example.nt118o211;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FrameLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_layout);
+    }
+}
---
+++ Lab_1/NT118.O21.1/app/src/main/java/com/example/nt118o211/RelativeLayoutTH.java
@@ -0,0 +1,14 @@
+package com.example.nt118o211;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelativeLayoutTH extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout_th);
+    }
+}
