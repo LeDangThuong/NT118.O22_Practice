6f0b827cf0a8a0eaa7383f39969652deee549b37
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-05-15 00:49:18 +0700

    Check lại mọi thứ, đều ổn, có thể tải về

--- lab_3/app/src/main/java/com/example/lab_3/Phan2/StudentActivity.java
+++ lab_3/app/src/main/java/com/example/lab_3/Phan2/StudentActivity.java
@@ -18,60 +18,61 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 
-public class StudentActivity  extends AppCompatActivity {
+public class StudentActivity extends AppCompatActivity {
     String DB_PATH_SUFFIX = "/databases/";
     SQLiteDatabase database = null;
-    String DATABASE_NAME = "qlsv1.db";
+    String DATABASE_NAME = "qlsv.db";
     //Khai báo ListView
     ListView lv;
-    ArrayList<String> mylist;
+    ArrayList<String> mylist = new ArrayList<>();
     ArrayAdapter<String> myadapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_listview);
-
-        database = openOrCreateDatabase("qlsv1.db", MODE_PRIVATE, null);
-
+//Ham Copy CSDL từ assets vào thư mục Databases
+        processCopy();
+//Mo CSDL trong ung dung len
+        database = openOrCreateDatabase("qlsv.db",MODE_PRIVATE, null);
+// Tạo ListView
         lv = findViewById(R.id.listview);
-        mylist = new ArrayList<>();
         myadapter = new ArrayAdapter<>(StudentActivity.this,
                 android.R.layout.simple_list_item_1, mylist);
         lv.setAdapter(myadapter);
         // Truy vấn CSDL và cập nhật hiển thị lên Listview
-        Cursor c = database.query("qlsv", null, null, null, null, null, null);
+        Cursor c =
+                database.query("qlsv",null,null,null,null,null,null);
         c.moveToFirst();
         String data = "";
-        while (c.isAfterLast() == false) {
-            data = c.getString(0) + " - " + c.getString(1) + " - "+c.getString(2);
+        while (c.isAfterLast() == false)
+        {
+            data = c.getString(0)+"-"+c.getString(1)+" "+c.getString(2);
             mylist.add(data);
             c.moveToNext();
         }
         c.close();
         myadapter.notifyDataSetChanged();
     }
-
     private void processCopy() {
         //private app
         File dbFile = getDatabasePath(DATABASE_NAME);
-        if (!dbFile.exists()) {
-            try {
-                CopyDataBaseFromAsset();
-                Toast.makeText(this, "Copying sucess from Assets folder",
-                        Toast.LENGTH_LONG).show();
-            } catch (Exception e) {
-                Toast.makeText(this, e.toString(),
-                        Toast.LENGTH_LONG).show();
-            }
+        if (dbFile.exists())
+            dbFile.delete(); // Xóa file cũ nếu đã tồn tại
+        try {
+            CopyDataBaseFromAsset();
+            Toast.makeText(this, "Copying success from Assets folder",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(this, e.toString(),
+                    Toast.LENGTH_LONG).show();
         }
     }
-
     private String getDatabasePath() {
-        return getApplicationInfo().dataDir + DB_PATH_SUFFIX +
+        return getApplicationInfo().dataDir + DB_PATH_SUFFIX+
                 DATABASE_NAME;
     }
-
+    // Ham copy file DB tu thu muc Asset vao file DB moi tao ra trong
+    //ung dung
     public void CopyDataBaseFromAsset() {
         try {
             InputStream myInput;
